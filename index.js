c686164d15db9cde14eeb7effe0d06ee2a933269
Romina Franco <rominaefranco@gmail.com>
2023-03-18 19:50:51 -0300

    preEntrega2

--- index.js
+++ index.js
@@ -6,52 +6,50 @@ class Banco {
         this.tasa24 = tasa24
     }
 }
-const santander = new Banco("santander",1,10,11)
-const frances = new Banco("frances",2,13,14)
-const provincia = new Banco("provincia",3,4,5)
+
+const bancos = [
+    new Banco('santander', 1, 10, 11),
+    new Banco('frances', 2, 13, 14),
+    new Banco('provincia', 3, 4, 5),
+]
 
 let seleccioneBanco = parseInt(
     prompt(
-        "selecciona tu Banco 1.Santander 2.Frances 3.Provincia"
+        'selecciona tu Banco 1.Santander 2.Frances 3.Provincia'
     )
 )
 
 let seleccionoBanco = false
 
-let infoBancoSeleccionado
+let banco
 
 while (seleccionoBanco === false) {
-    if (seleccioneBanco === 1) {
-        seleccionoBanco = true
-        infoBancoSeleccionado = santander
-    } else if (seleccioneBanco === 2) {
-        seleccionoBanco = true
-        infoBancoSeleccionado = frances
-    } else if (seleccioneBanco === 3) {
-        seleccionoBanco = true
-        infoBancoSeleccionado = provincia
-    } else {
-        seleccionoBanco = parseInt(
+    banco = bancos.find((banco) => banco.id === seleccioneBanco)
+    if (!banco) {
+        seleccioneBanco = parseInt(
             prompt(
                 'selecciona un numero CORRECTO 1.Santander 2.Frances 3.Provincia'
             )
         )
+    } else {
+        seleccionoBanco = true
     }
 }
 
-const importeSolicitado = parseInt(prompt('Ingresa el importe de tu prestamo personal'))
+const importeSolicitado = parseInt(
+    prompt('Ingresa el importe de tu prestamo personal')
+)
 
-const cuota12 = calculadorCuota(importeSolicitado, infoBancoSeleccionado.tasa12, 12)
-const cuota24 = calculadorCuota(importeSolicitado, infoBancoSeleccionado.tasa24, 24)
-console.log(cuota12, cuota24)
+const cuota12 = calculadorCuota(importeSolicitado, banco.tasa12, 12)
+const cuota24 = calculadorCuota(importeSolicitado, banco.tasa24, 24)
 
-function calculadorCuota(monto, interes, meses){
-    const cuota = (monto + (monto * interes)) / 100 / meses
+function calculadorCuota(monto, interes, meses) {
+    const cuota = (monto + monto * interes) / 100 / meses
     return cuota
 }
 
 alert(
-    `Las opciones disponibles en ${infoBancoSeleccionado.nombre} son: 1. ${cuota12} mensuales a 12 meses - 2. ${cuota24} mensuales a 24 meses`
+    `Las opciones disponibles en ${banco.nombre} son: 1. ${cuota12} mensuales a 12 meses - 2. ${cuota24} mensuales a 24 meses`
 )
-console.log('seleccioneBanco', infoBancoSeleccionado, importeSolicitado)
+
 
